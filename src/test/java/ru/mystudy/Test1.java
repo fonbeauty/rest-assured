3d8d2e38022ba34546e3e6ba66fb3eac75ff8555
fonbeauty <fonbeauty@mail.ru>
2019-03-25 23:42:26 +0300

    Доработка логирования

--- src/test/java/ru/mystudy/Test1.java
+++ src/test/java/ru/mystudy/Test1.java
@@ -1,5 +1,11 @@
 package ru.mystudy;
 
+import io.restassured.RestAssured;
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.builder.ResponseSpecBuilder;
+import io.restassured.filter.log.LogDetail;
+import io.restassured.specification.RequestSpecification;
+import io.restassured.specification.ResponseSpecification;
 import org.testng.annotations.Test;
 
 import static io.restassured.RestAssured.given;
@@ -7,9 +13,23 @@ import static io.restassured.RestAssured.given;
 public class Test1 {
     @Test
     public void test1() {
-        given().baseUri("https://swapi.co/")
-            .when().get("api/people/1")
-            .then().statusCode(200);
+
+        RestAssured.baseURI = "https://swapi.co/";
+
+        RequestSpecification requestSpecification = new RequestSpecBuilder()
+//                .setBaseUri("https://swapi.co/")
+                .log(LogDetail.ALL)
+                .build();
+
+
+        ResponseSpecification responseSpecification = new ResponseSpecBuilder()
+                .expectStatusCode(200).build();
+
+        RestAssured.responseSpecification = responseSpecification;
+
+        given().spec(requestSpecification)
+            .when().get("api/people/1");
+//            .then().log().all().statusCode(200);
 
 
 
